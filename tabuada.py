0c13b3f6dd1ec272d9245da793342671ed07249d
Matheus Vargas <matheusvargas042@gmail.com>
2023-08-11 18:56:15 -0300

    formatando o retorno da tabuada.

--- tabuada.py
+++ tabuada.py
@@ -1,11 +1,13 @@
 """Tabuada do 1 a 10."""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Matheus"
 
 numeros = list(range(1, 11))
 
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(outro_numero * numero)
-    print("---------")
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} X {n2} = {resultado}"))
+    print("#" * 18)
