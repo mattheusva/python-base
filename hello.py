15aa004abe9c48e13409bb8b8b3613c941e6d207
Matheus Vargas <matheusvargas042@gmail.com>
2023-10-04 18:49:00 -0300

    aula input output

--- hello.py
+++ hello.py
@@ -9,16 +9,39 @@ Tenha a variável LANG devidamente configurada ex:
 
     LANG=pt_BR
 
+ou informe através da CLI arguments "--lang"
+
+ou o usuário terá que digitar.
 Execução:
 
     python hello.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Matheus"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5] #primeiros 5 char
+arguments = {
+    "lang": None,
+    "count": 1
+}
+for arg in sys.argv[1:]:
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid option `{key}`")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    current_language = os.getenv("LANG")
+    if current_language is None:
+        current_language = input("Choose a language:")
+
+current_language = current_language[:5]
 
 # Dicts, Sets, Hash Table - O(1) - Constante
 
@@ -30,4 +53,4 @@ msg = {
     "es_SP": "Hola, Mondo!",
 }
 
-print(msg[current_language])
+print(msg[current_language] * int(arguments["count"]))
